0857c81d4f6029b0780382b6fa5047c0ac00589a
Bhuvan <bhuvanesh.dongarwar@gmail.com>
2026-06-30 10:22:01 +0530

    Create maven project structure

---
+++ src/main/java/com/airtribe/ridewise/Main.java
@@ -0,0 +1,11 @@
+package com.airtribe.ridewise;
+
+/**
+ * Entry point for the RideWise application.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("=== RideWise Application ===");
+    }
+}
